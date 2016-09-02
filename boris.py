2b84c0bc564bf037d9ed284b27a2df7ac94cb953
olivierfriard <olivier.friard@unito.it>
2016-09-02 10:47:20 +0200

    version 2.99

--- boris.py
+++ boris.py
@@ -23,8 +23,8 @@ This file is part of BORIS.
 """
 
 
-__version__ = "2.983"
-__version_date__ = "2016-09-01"
+__version__ = "2.99"
+__version_date__ = "2016-09-02"
 __DEV__ = False
 BITMAP_EXT = "jpg"
 
