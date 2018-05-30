d64e9f85c3f3be57e97a4f3757d2002717302e2d
Olivier Friard <olivier.friard@unito.it>
2018-05-30 13:50:08 +0200

    changed version

--- boris.py
+++ boris.py
@@ -103,8 +103,8 @@ import export_observation
 import time_budget_functions
 
 
-__version__ = "6.3.0"
-__version_date__ = "2018-05-28"
+__version__ = "6.3.1"
+__version_date__ = "2018-05-29"
 
 if platform.python_version() < "3.5":
     logging.critical("BORIS requires Python 3.5+! You are using v. {}")
