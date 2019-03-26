cdbd3d28d75b367ba5e0bf1028bb4699b6cd38eb
Olivier Friard <olivier.friard@unito.it>
2019-03-26 16:18:12 +0100

    work on needleman-wunsch identity

--- src/irr.py
+++ src/irr.py
@@ -28,11 +28,63 @@ import numpy as np
 from config import *
 
 
+def subj_behav_modif(cursor, obsid, subject, time, interval, include_modifiers):
+    """
+    current behaviors for observation obsId at time
+
+    Args:
+        cursor (sqlite3.cursor): cursor to aggregated events db
+        obsid (str): id of observation
+        subject (str): name of subject
+        time (Decimal): time
+        include_modifiers (bool): True: include modifiers False: do not
+
+    Returns:
+        list: list of lists [subject, behavior, modifiers]
+    """
+
+    s = []
+    # state behaviors
+    rows = cursor.execute("""SELECT behavior, modifiers FROM aggregated_events
+              WHERE
+               observation = ?
+               AND subject = ?
+               AND type = 'STATE'
+               AND (? BETWEEN start AND STOP)
+               """ ,
+            (obsid, subject, float(time),)).fetchall()
+
+    for row in rows:
+        if include_modifiers:
+            s.append([subject, row[0], row[1]])
+        else:
+            s.append([subject, row[0]])
+
+    # point behaviors
+    rows = cursor.execute("""SELECT behavior, modifiers FROM aggregated_events
+              WHERE
+               observation = ?
+               AND subject = ?
+               AND type = 'POINT'
+               AND abs(start - ?) <= ?
+               """ ,
+            (obsid, subject, float(time), float(interval / 2),)).fetchall()
+
+    for row in rows:
+        if include_modifiers:
+            s.append([subject, row[0], row[1]])
+        else:
+            s.append([subject, row[0]])
+
+    return s
+
+
 def cohen_kappa(cursor,
-                obsid1, obsid2,
-                interval,
-                selected_subjects,
-                include_modifiers):
+                obsid1:str,
+                obsid2:str,
+                interval:float,
+                selected_subjects:list,
+                include_modifiers:bool):
     """
     Inter-rater reliability Cohen's kappa coefficient (time-unit)
     see Sequential Analysis and Observational Methods for the Behavioral Sciences p. 77
@@ -42,6 +94,7 @@ def cohen_kappa(cursor,
         cursor (sqlite3.cursor): cursor to aggregated events db
         obsid1 (str): id of observation #1
         obsid2 (str): id of observation #2
+        interval (float): time unit (s)
         selected_subjects (list): subjects selected for analysis
         include_modifiers (bool): True: include modifiers False: do not
 
@@ -49,56 +102,6 @@ def cohen_kappa(cursor,
         float: K
         str: result of analysis
     """
-
-    def subj_behav_modif(cursor, obsid, subject, time, include_modifiers):
-        """
-        current behaviors for observation obsId at time
-
-        Args:
-            cursor (sqlite3.cursor): cursor to aggregated events db
-            obsid (str): id of observation
-            subject (str): name of subject
-            time (Decimal): time
-            include_modifiers (bool): True: include modifiers False: do not
-
-        Returns:
-            list: list of lists [subject, behavior, modifiers]
-        """
-
-        s = []
-        # state behaviors
-        rows = cursor.execute("""SELECT behavior, modifiers FROM aggregated_events
-                  WHERE
-                   observation = ?
-                   AND subject = ?
-                   AND type = 'STATE'
-                   AND (? BETWEEN start AND STOP)
-                   """ ,
-                (obsid, subject, float(currentTime),)).fetchall()
-
-        for row in rows:
-            if include_modifiers:
-                s.append([subject, row[0], row[1]])
-            else:
-                s.append([subject, row[0]])
-
-        # point behaviors
-        rows = cursor.execute("""SELECT behavior, modifiers FROM aggregated_events
-                  WHERE
-                   observation = ?
-                   AND subject = ?
-                   AND type = 'POINT'
-                   AND abs(start - ?) <= ?
-                   """ ,
-                (obsid, subject, float(currentTime), float(interval / 2),)).fetchall()
-
-        for row in rows:
-            if include_modifiers:
-                s.append([subject, row[0], row[1]])
-            else:
-                s.append([subject, row[0]])
-
-        return s
 
     first_event = cursor.execute(("SELECT min(start) FROM aggregated_events "
                                   "WHERE observation in (?, ?) AND subject in ('{}') ").format("','".join(selected_subjects)),
@@ -130,7 +133,7 @@ def cohen_kappa(cursor,
         for obsid in [obsid1, obsid2]:
             for subject in selected_subjects:
 
-                s = subj_behav_modif(cursor, obsid, subject, currentTime, include_modifiers)
+                s = subj_behav_modif(cursor, obsid, subject, currentTime, interval, include_modifiers)
 
                 if s not in total_states:
                     total_states.append(s)
@@ -139,20 +142,26 @@ def cohen_kappa(cursor,
 
         currentTime += interval
 
-
     total_states = sorted(total_states)
 
     logging.debug(f"total_states: {total_states} len:{len(total_states)}")
 
     contingency_table = np.zeros((len(total_states), len(total_states)))
 
+    seq1 = {}
+    seq2 = {}
     currentTime = Decimal(str(first_event))
     while currentTime <= last_event:
 
+        seq1[currentTime] = []
+        seq2[currentTime] = []
         for subject in selected_subjects:
 
-            s1 = subj_behav_modif(cursor, obsid1, subject, currentTime, include_modifiers)
-            s2 = subj_behav_modif(cursor, obsid2, subject, currentTime, include_modifiers)
+            s1 = subj_behav_modif(cursor, obsid1, subject, currentTime, interval, include_modifiers)
+            s2 = subj_behav_modif(cursor, obsid2, subject, currentTime, interval, include_modifiers)
+
+            seq1[currentTime].append(s1)
+            seq2[currentTime].append(s2)
 
             logging.debug(f"currentTime: {currentTime} s1:{s1} s2:{s2}")
 
@@ -162,6 +171,9 @@ def cohen_kappa(cursor,
                 return -100, "Error with contingency table"
 
         currentTime += interval
+
+    logging.debug(f"seq1:\n {list(seq1.values())}")
+    logging.debug(f"seq2:\n {list(seq2.values())}")
 
     logging.debug("contingency_table:\n {}".format(contingency_table))
 
@@ -209,4 +221,231 @@ def cohen_kappa(cursor,
     logging.debug(f"K: {K}")
     return K, out
 
+
+def needleman_wunsch_identity(cursor,
+                              obsid1, obsid2,
+                              interval,
+                              selected_subjects,
+                              include_modifiers):
+    """
+    Needleman - Wunsch alignment
+
+    Args:
+        cursor (sqlite3.cursor): cursor to aggregated events db
+        obsid1 (str): id of observation #1
+        obsid2 (str): id of observation #2
+        selected_subjects (list): subjects selected for analysis
+        include_modifiers (bool): True: include modifiers False: do not
+
+    Return:
+        float: K
+        str: result of analysis
+    """
+
+
+    def zeros(shape):
+        retval = []
+        for x in range(shape[0]):
+            retval.append([])
+            for y in range(shape[1]):
+                retval[-1].append(0)
+        return retval
+
+    match_award      = 1
+    mismatch_penalty = -1
+    gap_penalty      = -1 # both for opening and extanding
+
+
+    def match_score(alpha, beta):
+        if alpha == beta:
+            return match_award
+        elif alpha == '-' or beta == '-':
+            return gap_penalty
+        else:
+            return mismatch_penalty
+
+
+    def finalize(align1, align2):
+        align1 = align1[::-1]    #reverse sequence 1
+        align2 = align2[::-1]    #reverse sequence 2
+
+        i,j = 0,0
+
+        #calcuate identity, score and aligned sequeces
+        symbol = []
+        found = 0
+        score = 0
+        identity = 0
+        for i in range(0,len(align1)):
+            # if two AAs are the same, then output the letter
+            if align1[i] == align2[i]:
+                symbol.append(align1[i])
+                identity = identity + 1
+                score += match_score(align1[i], align2[i])
+
+            # if they are not identical and none of them is gap
+            elif align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-':
+                score += match_score(align1[i], align2[i])
+                symbol.append(" ")
+                found = 0
+
+            #if one of them is a gap, output a space
+            elif align1[i] == '-' or align2[i] == '-':
+                symbol.append(" ")
+                score += gap_penalty
+
+        identity = float(identity) / len(align1) * 100
+
+        return {'identity': identity,
+                'score': score,
+                "align1": align1,
+                "align2": align2,
+                "symbol": symbol}
+
+
+    def needle(seq1, seq2):
+        m, n = len(seq1), len(seq2)  # length of two sequences
+
+        # Generate DP table and traceback path pointer matrix
+        score = zeros((m + 1, n + 1))      # the DP table
+
+        # Calculate DP table
+        for i in range(0, m + 1):
+            score[i][0] = gap_penalty * i
+        for j in range(0, n + 1):
+            score[0][j] = gap_penalty * j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                match = score[i - 1][j - 1] + match_score(seq1[i - 1], seq2[j - 1])
+                delete = score[i - 1][j] + gap_penalty
+                insert = score[i][j - 1] + gap_penalty
+                score[i][j] = max(match, delete, insert)
+
+        # Traceback and compute the alignment
+        align1, align2 = [], []
+        i,j = m,n # start from the bottom right cell
+        while i > 0 and j > 0: # end toching the top or the left edge
+            score_current = score[i][j]
+            score_diagonal = score[i-1][j-1]
+            score_up = score[i][j-1]
+            score_left = score[i-1][j]
+
+            if score_current == score_diagonal + match_score(seq1[i-1], seq2[j-1]):
+                align1.append(seq1[i-1])
+                align2.append(seq2[j-1])
+                i -= 1
+                j -= 1
+            elif score_current == score_left + gap_penalty:
+                align1.append(seq1[i-1])
+                align2.append("-")
+                i -= 1
+            elif score_current == score_up + gap_penalty:
+                align1.append("-")
+                align2.append(seq2[j-1])
+                j -= 1
+
+        # Finish tracing up to the top left cell
+        while i > 0:
+            align1.append(seq1[i-1])
+            align2.append("-")
+            i -= 1
+        while j > 0:
+            align1.append("-")
+            align2.append(seq2[j-1])
+            j -= 1
+
+        return finalize(align1, align2)
+
+
+
+
+    first_event = cursor.execute(("SELECT min(start) FROM aggregated_events "
+                                  "WHERE observation in (?, ?) AND subject in ('{}') ").format("','".join(selected_subjects)),
+                                                                                               (obsid1, obsid2)).fetchone()[0]
+
+    if first_event is None:
+        logging.debug(f"An observation has no recorded events: {obsid1} or {obsid2}")
+        return -100, f"An observation has no recorded events: {obsid1} {obsid2}"
+
+    logging.debug(f"first_event: {first_event}")
+    last_event = cursor.execute(("SELECT max(stop) FROM aggregated_events "
+                                 "WHERE observation in (?, ?) AND subject in ('{}') ").format("','".join(selected_subjects)),
+                                                                                             (obsid1, obsid2)).fetchone()[0]
+
+    logging.debug("last_event: {}".format(last_event))
+
+    nb_events1 = cursor.execute(("SELECT COUNT(*) FROM aggregated_events "
+                                 "WHERE observation = ? AND subject in ('{}') ").format("','".join(selected_subjects)),
+                                                                                        (obsid1,)).fetchone()[0]
+    nb_events2 = cursor.execute(("SELECT COUNT(*) FROM aggregated_events "
+                                 "WHERE observation = ? AND subject in ('{}') ").format("','".join(selected_subjects)),
+                                                                                       (obsid2,)).fetchone()[0]
+
+    # check number of different states
+    '''
+    total_states = []
+
+    currentTime = Decimal(str(first_event))
+    while currentTime <= last_event:
+
+        for obsid in [obsid1, obsid2]:
+            for subject in selected_subjects:
+
+                s = subj_behav_modif(cursor, obsid, subject, currentTime, interval, include_modifiers)
+
+                if s not in total_states:
+                    total_states.append(s)
+
+                logging.debug(f"{obsid} {subject} {currentTime} {s}")
+
+        currentTime += interval
+
+    total_states = sorted(total_states)
+
+    logging.debug(f"total_states: {total_states} len:{len(total_states)}")
+
+    contingency_table = np.zeros((len(total_states), len(total_states)))
+    '''
+
+    seq1 = {}
+    seq2 = {}
+    currentTime = Decimal(str(first_event))
+    while currentTime <= last_event:
+
+        seq1[currentTime] = []
+        seq2[currentTime] = []
+        for subject in selected_subjects:
+
+            s1 = subj_behav_modif(cursor, obsid1, subject, currentTime, interval, include_modifiers)
+            s2 = subj_behav_modif(cursor, obsid2, subject, currentTime, interval, include_modifiers)
+
+            seq1[currentTime].append(s1)
+            seq2[currentTime].append(s2)
+
+            logging.debug(f"currentTime: {currentTime} s1:{s1} s2:{s2}")
+
+            '''
+            try:
+                contingency_table[total_states.index(s1), total_states.index(s2)] += 1
+            except:
+                return -100, "Error with contingency table"
+            '''
+
+        currentTime += interval
+
+    logging.debug(f"seq1:\n {list(seq1.values())}")
+    logging.debug(f"seq2:\n {list(seq2.values())}")
+
+    '''logging.debug("contingency_table:\n {}".format(contingency_table))'''
+
+    r = needle(list(seq1.values()),list(seq2.values()) )
+
+    out = (f"Observation: {obsid1}\n"
+           f"number of events: {nb_events1}\n\n"
+           f"Observation: {obsid2}\n"
+           f"number of events: {nb_events2:.0f}\n\n"
+           f"identity = {r['identity']:.3f}")
+
+    logging.debug(f"identity: {r['identity']}")
+    return r['identity'], out
 
