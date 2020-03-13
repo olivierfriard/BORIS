61a5365f19171fa537441ef2044d14d5cd8ab0ed
Olivier Friard <olivier.friard@unito.it>
2020-03-13 17:14:03 +0100

    create pip

---
+++ src/test_pynput.py
@@ -0,0 +1,27 @@
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+# Read pointer position
+print('The current pointer position is {0}'.format(
+    mouse.position))
+
+# Set pointer position
+mouse.position = (10, 20)
+print('Now we have moved it to {0}'.format(
+    mouse.position))
+
+# Move pointer relative to current position
+mouse.move(5, -5)
+
+# Press and release
+mouse.press(Button.left)
+mouse.release(Button.left)
+
+# Double click; this is different from pressing and releasing
+# twice on Mac OSX
+mouse.click(Button.left, 2)
+
+# Scroll two steps down
+mouse.scroll(0, 2)
+
