0c3f1e1651f28d2f3f6a9c2ae7c587d62cf71511
Olivier Friard <olivier.friard@unito.it>
2022-03-17 16:26:34 +0100

    some work

--- boris/version.py
+++ boris/version.py
@@ -17,7 +17,6 @@ This file is part of BORIS.
 
   You should have received a copy of the GNU General Public License
   along with this program; if not see <http://www.gnu.org/licenses/>.
-
 """
 
 __version__ = "7.13.1"
