ff96055cf7230233f387ab03384e2d0c4ba829f3
Olivier Friard <olivier.friard@unito.it>
2022-05-12 16:59:15 +0200

    some changes

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
