ba2db79530e47df4fe1ad2268f5910024495bf5b
Olivier Friard <olivier.friard@unito.it>
2022-05-18 20:51:00 +0200

    some work

--- setup.py
+++ setup.py
@@ -3,46 +3,54 @@
 from setuptools import setup
 
 setup(
-   name='boris-behav-obs',
-   version=[x for x in open("boris/version.py","r").read().split("\n") if "__version__" in x][0].split(" = ")[1].replace('"', ''),
-   description='BORIS',
-   author='Olivier Friard - Marco Gamba',
-   author_email='olivier.friard@unito.it',
-   long_description=open("README_pip.rst", "r").read(),
-   #long_description_content_type="text/markdown",
-   url="http://www.boris.unito.it",
-   python_requires=">=3.6",
-   classifiers=[
+    name="boris-behav-obs",
+    version=[x for x in open("boris/version.py", "r").read().split("\n") if "__version__" in x][0]
+    .split(" = ")[1]
+    .replace('"', ""),
+    description="BORIS",
+    author="Olivier Friard - Marco Gamba",
+    author_email="olivier.friard@unito.it",
+    long_description=open("README_pip.rst", "r").read(),
+    # long_description_content_type="text/markdown",
+    url="http://www.boris.unito.it",
+    python_requires=">=3.6",
+    classifiers=[
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    packages=['boris'],  #same as name
-
+    packages=["boris"],  # same as name
     install_requires=[
-          "pillow>=9.0.1",
-          "numpy>=1.21",
-          "matplotlib==3.3.3",
-          "tablib[html, ods, xls, xlsx]",
-          "pyqt5==5.15.4",
-      ],
-
+        "pillow>=9.0.1",
+        "numpy>=1.21",
+        "matplotlib==3.3.3",
+        "tablib[html, ods, xls, xlsx]==3.2.1",
+        "pyqt5==5.15.4",
+    ],
     package_data={
-    'boris': ['core.qrc', 'core.ui', 'add_modifier.ui', 'converters.ui', 'edit_event.ui', 'observation.ui', 'param_panel.ui', 'preferences.ui', 'project.ui',
-              'portion/*.py'],
-     "": ["README.TXT", "LICENSE.TXT"],
-    },
-
-    entry_points={
-        'console_scripts': [
-            'boris = boris:main',
+        "boris": [
+            "core.qrc",
+            "core.ui",
+            "add_modifier.ui",
+            "converters.ui",
+            "edit_event.ui",
+            "observation.ui",
+            "param_panel.ui",
+            "preferences.ui",
+            "project.ui",
+            "portion/*.py",
         ],
-    }
- )
-
+        "": ["README.TXT", "LICENSE.TXT"],
+    },
+    entry_points={
+        "console_scripts": [
+            "boris = boris:main",
+        ],
+    },
+)
