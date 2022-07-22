54e68045c059858484fd2cc032af751ae02e27ef
Olivier Friard <olivier.friard@unito.it>
2022-07-22 18:25:07 +0200

    some changes

--- boris/observation_ui.py
+++ boris/observation_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'boris/observation.ui'
+# Form implementation generated from reading ui file 'observation.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
