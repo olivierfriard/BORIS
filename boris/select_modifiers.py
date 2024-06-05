25d70677028ae24c036f0817e19b5b7784c9125d
Knöll <jonas.knoell@fli.de>
2024-06-05 16:29:29 +0200

    fixes moving focus onto cancel button when last modifier is a single selection modifier intruduced in SHA:5986bee596f4b2fc1a975a97dba235b52e13c3ed

--- boris/select_modifiers.py
+++ boris/select_modifiers.py
@@ -114,6 +114,7 @@ class ModifiersList(QDialog):
             if self.modifiers_dict[idx]["type"] in [cfg.NUMERIC_MODIFIER]:
                 le = QLineEdit()
                 self.modifiers_dict[idx]["widget"] = le
+                le.setObjectName(f"le_modifiers_({self.modifiers_dict[idx]['type']})")
 
                 if currentModifierList != [""] and currentModifierList[int(idx)] != "None":
                     le.setText(currentModifierList[int(idx)])
@@ -163,7 +164,10 @@ class ModifiersList(QDialog):
                 self.accept()
                 return True
 
+            modifiersSetIndex=0
             for widget in self.children():
+                if "_modifiers" in widget.objectName():
+                    modifiersSetIndex = modifiersSetIndex + 1
                 if "lw_modifiers" in widget.objectName():
                     if self.modifiersSetNumber == 1:
                         # check if modifiers have code
@@ -197,6 +201,7 @@ class ModifiersList(QDialog):
                                 except Exception:
                                     return
 
+
                     for index in range(widget.count()):
                         if ek in cfg.function_keys:
                             if f"({cfg.function_keys[ek]})" in widget.item(index).text().upper():
@@ -207,7 +212,7 @@ class ModifiersList(QDialog):
                                         self.accept()
                                         return True
                                     #else move to next set of mofifiers
-                                    elif index!=self.modifiersSetNumber:
+                                    elif modifiersSetIndex!=self.modifiersSetNumber:
                                         widget.parent().focusNextChild()
                                         return True
 
@@ -225,7 +230,7 @@ class ModifiersList(QDialog):
                                     self.accept()
                                     return True
                                 #else move to next set of mofifiers
-                                elif index!=self.modifiersSetNumber:
+                                elif modifiersSetIndex!=self.modifiersSetNumber:
                                         widget.parent().focusNextChild()
                                         return True
 
