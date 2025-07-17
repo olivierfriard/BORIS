ea99716403102a46a9a801457c85af0822ea6bdd
Olivier Friard <olivier.friard@unito.it>
2025-07-17 15:15:39 +0200

    fixed plugin "number of occurences of behaviours by subject by independent variable"

--- boris/analysis_plugins/number_of_occurences_by_independent_variable.py
+++ boris/analysis_plugins/number_of_occurences_by_independent_variable.py
@@ -6,8 +6,8 @@ number of occurences of behaviors by independent_variable
 
 import pandas as pd
 
-__version__ = "0.3.0"
-__version_date__ = "2025-03-17"
+__version__ = "0.4.0"
+__version_date__ = "2025-07-17"
 __plugin_name__ = "Number of occurences of behaviors by subject by independent_variable"
 __author__ = "Olivier Friard - University of Torino - Italy"
 
@@ -19,16 +19,36 @@ def run(df: pd.DataFrame):
     This plugin returns a Pandas dataframe
     """
 
-    df_results: df.DataFrame = (
-        df.groupby(
-            [
-                "independent variable 'Weather'",
-                "Subject",
-                "Behavior",
-            ]
-        )["Behavior"]
-        .count()
-        .reset_index(name="number of occurences")
-    )
+    df_results_list: list = []
 
-    return df_results
+    flag_variable_found = False
+
+    for column in df.columns:
+        if isinstance(column, tuple) or (isinstance(column, str) and not column.startswith("independent variable '")):
+            continue
+
+        flag_variable_found = True
+        grouped_df: df.DataFrame = (
+            df.groupby(
+                [
+                    column,
+                    "Subject",
+                    "Behavior",
+                ]
+            )["Behavior"]
+            .count()
+            .reset_index(name="number of occurences")
+        )
+
+        grouped_df.rename(columns={column: "Value"}, inplace=True)
+
+        grouped_df.insert(0, "independent variable name", column)
+
+        df_results_list.append(grouped_df)
+
+    df_results = pd.concat(df_results_list, ignore_index=True) if df_results_list else pd.DataFrame([])
+
+    if not flag_variable_found:
+        return "No independent variable found"
+    else:
+        return df_results
--- boris/version.py
+++ boris/version.py
@@ -20,5 +20,5 @@ This file is part of BORIS.
 
 """
 
-__version__ = "9.6"
-__version_date__ = "2025-07-15"
+__version__ = "9.6.1"
+__version_date__ = "2025-07-17"
